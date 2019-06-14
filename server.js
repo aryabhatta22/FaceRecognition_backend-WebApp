2503c61976190db2645badf4b500768157c3d705
Tarun Sharma <tarun.hive@hotmail.com>
2019-06-14 17:19:37 +0530

    initial commits, backend connected to frontend

---
+++ server.js
@@ -0,0 +1,153 @@
+const express=require('express');
+const Clarifai=require('clarifai');
+const bodyParser =require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+
+
+const db=knex({
+   client: 'pg' ,
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'unitedwestand',
+        database: 'smart_brain'
+    }
+});
+
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req,res)=> {
+    res.json(db.users);
+})
+
+//Signin
+
+app.post('/signin', (req,res)=> {
+    
+    const {email, password} =req.body;
+    if(!email || !password) {
+        return res.status(400).json('incorrect form submission');
+    }
+    
+    db.select('email', 'hash').from('login')
+        .where('email','=', email)
+        .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if(isValid){
+            return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+//Register
+
+app.post('/register', (req, res) => {
+    
+    const {email, name, password } =req.body;
+    
+    if(!email || !name || !password) {
+        return res.status(400).json('incorrect form submission')
+    }
+    
+    const hash=bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user=>{
+            res.json(user[0]);
+        })
+     })
+    .then(trx.commit)
+    .catch(trx.rollback)
+    })
+  
+    .catch(err=> res.status(400).json('unable to join')); //if use err instead of 'unable to join' it will return users information 
+    
+    
+})
+
+
+//Profile
+
+app.get('/profile/:id', (req,res) => {
+    const {id }=req.params;
+    db.select('*').from('users').where({
+        id: id
+    }).then(user=> {
+        if(user.length){
+            res.json(user[0])    
+        }
+        else {
+            res.status(400).json('Not found')
+        }
+    })
+    .catch(err=> res.status(400).json('error getting user'))
+    
+})
+
+//Image (update)
+
+app.put('/image', (req, res) => {
+     const {id }=req.body;
+    db('users').where('id','=', id)
+        .increment('enteries', 1)
+        .returning('enteries')
+        .then(enteries => {
+        res.json(enteries[0]);
+    })
+    .catch(err => {
+        res.status(400).json('unable to get entries');
+    })
+})
+
+
+//imageURL
+
+
+const apiApp = new Clarifai.App({
+    apiKey: '636faf6222004476b96b13b0b4c9a90f'
+});
+
+
+app.post('/imageurl', (req,res)=>{
+    apiApp.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+    res.json(data);
+})
+    .catch(err => res.status(400).json('unable to fetch informations '))
+
+})
+app.listen(3001,()=>{
+    console.log('app is running on port 3001')
+})
+
+
